c20a64ca3685a32e0913101445d4bdc93ec3dd26
tcneri <thalitacostaneri@gmail.com>
2020-09-15 21:45:54 -0300

    Exemplo 3 - Estoque

---
+++ src/br/com/digitalhouse/exEstoque/Principal.kt
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.exEstoque
+
+fun main(){
+    val estoque1 = Estoque("café",20,5)
+
+    estoque1.mostra()
+    estoque1.mudarNome("feijao")
+    estoque1.darBaixa(5)
+    println(estoque1.precisaRepor())
+    estoque1.repor(10)
+    estoque1.mudarQtdMinima(2)
+
+}
