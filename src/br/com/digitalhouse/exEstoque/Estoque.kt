6981948bea137c46410e355f3b4fc026f5ae14a7
gLeobonacini <70539330+gLeobonacini@users.noreply.github.com>
2020-09-15 22:36:32 -0300

    Adicionando estoque

--- src/br/com/digitalhouse/exEstoque/Estoque.kt
+++ src/br/com/digitalhouse/exEstoque/Estoque.kt
@@ -1,5 +1,43 @@
-package br.com.digitalhouse.exEstoque
+package br.com.digitalHouse.controleEstoque
 
-class Estoque(var nome: String, var qtdAtual: Int, var qtdMinima: Int) {
+class Estoque(var nome: String = "", var qtdAtual: Int = 0, var qtdMinima: Int = 0) {
+    init{
+        when(qtdAtual < 0){
+            true -> qtdAtual = 0
+        }
 
+        when(qtdMinima < 0){
+            true -> qtdMinima = 0
+        }
+    }
+
+    fun mudarNome(novoNome: String){
+        nome = novoNome
+        println("Nome: $nome")
+    }
+
+    fun mudarQtdMinima(novaQtdMinima: Int){
+        if (novaQtdMinima >= 0)
+            qtdMinima = novaQtdMinima
+        println("Quantidade mínima: $qtdMinima")
+    }
+
+    fun repor(qtd: Int){
+        qtdAtual += qtd
+        println("Quantidade atual: $qtdAtual")
+    }
+
+    fun darBaixa(qtd: Int){
+        if(qtd <= qtdAtual)
+            qtdAtual -= qtd
+        println("Quantidade atual: $qtdAtual")
+    }
+
+    fun mostra(): String{
+        return "O produto $nome possui como quantidade mínima $qtdMinima unidades e atualmente tem-se $qtdAtual unidades em estoque."
+    }
+
+    fun precisaRepor(): Boolean {
+        return qtdAtual <= qtdMinima
+    }
 }
